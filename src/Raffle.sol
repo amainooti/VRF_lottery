3883f93f6d5ea4ec3f59eee1416824f054aa7214
amainooti <amainosamuel1@gmail.com>
2025-04-01 17:23:32 +0100

    added the inital file

---
+++ src/Raffle.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+
+/**
+ * @title A sample of a raffle contract
+ * @author Amaino Oti
+ * @notice This Contract is for creating a sample raffle
+ * @dev Implements Chainlink VRF v2.0
+ */
+
+contract Rafflle {
+
+}
+
