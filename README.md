588e262dc55c4e77aa72b2adbb6a7b2eddd6bda7
amainooti <amainosamuel1@gmail.com>
2025-04-05 22:47:30 +0100

    added the VRFConsumer contract

--- README.md
+++ README.md
@@ -1,66 +1,17 @@
-## Foundry
+# Proveably Random Raffle Contract
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
 
-Foundry consists of:
+## About
 
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
 
-## Documentation
+This code is to create a proveably random smart contract lottery.
 
-https://book.getfoundry.sh/
+## What we want is to do?
 
-## Usage
-
-### Build
-
-```shell
-$ forge build
-```
-
-### Test
-
-```shell
-$ forge test
-```
-
-### Format
-
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
-### Anvil
-
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
+1. Users can enter by paying for a ticket
+    1. The ticket fees are going to go to the winners during the draw
+2. After x period of time, the lottery will automatically draw a winner
+    1. And this will be done programmatically
+3. Using chainlink VRF & chainlink automation
+    1. Chainlink VRF -> Randomness
+    2. Chainlink Automation -> Time Based Trigger
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,7 +1,25 @@
 // SPDX-License-Identifier: MIT
 
+// Layout of functions
+
+// constructor
+
+// receive function (if exists)
+
+// fallback function (if exists)
+
+// external
+
+// public
+
+// internal
+
+// private
+
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 /**
  * @title A sample of a raffle contract
@@ -10,7 +28,94 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRF v2.0
  */
 
-contract Rafflle {
+contract Rafflle is VRFConsumerBaseV2 {
+    error NotEnoughETHSent();
+    error NotEnoughTimePaassed();
 
+    // state variables
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    uint256 private immutable i_entranceFee;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address private s_recent_winner;
+
+    // Events
+    event EnteredRaffle(address indexed player);
+
+    constructor(
+        uint256 _entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+        i_entranceFee = _entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function enterraffles() external payable {
+        if (msg.value <= i_entranceFee) {
+            revert NotEnoughETHSent();
+        }
+
+        s_players.push(payable(msg.sender));
+        //  1. Makes Migration easier
+        //  2. Makes frontend indexing easier
+        emit EnteredRaffle(msg.sender);
+    }
+
+    /*
+    1. Get a random number
+    2. Use a random number to get a player
+    3. Be automatically called
+    
+    */
+    function pickWinner() public {
+        // Check if enough time has passed
+
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert NotEnoughTimePaassed();
+        }
+
+        //1. Request RGN <-- Random Number Generator
+        //2. Get a random number
+        //3. Use a random number to get a player
+        //4. Send them the money
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fufillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recent_winner = winner;
+        (bool success, ) = winner.call{value: address(this).balance}("");
+    }
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
-
