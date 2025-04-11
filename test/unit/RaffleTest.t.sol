fa19b4592bc2357831e35986b3cf5c4bb97fd104
amainooti <amainosamuel1@gmail.com>
2025-04-11 19:34:13 +0100

    added the deploy script

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.18;
+import {Test, console} from "forge-std/Test.sol" ;
+import {Raffle} from "../../src/Raffle.sol";
+import {Deployeraffle} from "../../script/Deployraffle.s.sol";
+contract RaffleTest is Test{
+
+    function setUp() external {
+
+    }
+}
